a87f56cb0d4cf03de04d62b0f6c4d7037c2d72f7
Mikael Östberg <ostberg.mikael@gmail.com>
2016-03-07 23:03:06 +0100

    Added copying web folder as a task

--- src/main/groovy/Pesto.groovy
+++ src/main/groovy/Pesto.groovy
@@ -1,6 +1,6 @@
 import static spark.Spark.*
 import static Utils.*;
-staticFileLocation '/src/web'
+staticFileLocation '/build/public'
 port 8080
 println "Configuring server on port 8080"
 println 'CWD: ' + System.getProperty('user.dir')
