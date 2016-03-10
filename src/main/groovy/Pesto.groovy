a6644bcbbc48b7dfa1f1c6f01b1b965e771d71da
Mikael Östberg <ostberg.mikael@gmail.com>
2016-03-10 23:57:03 +0100

    Enabled live source-development

--- src/main/groovy/Pesto.groovy
+++ src/main/groovy/Pesto.groovy
@@ -2,8 +2,7 @@ import static spark.Spark.*
 import static Utils.*;
 import java.io.File;
 
-String cwd = System.getProperty('user.dir')
-String serverHome = cwd + File.separator + 'build' + File.separator + 'public';
+String serverHome = System.getProperty('user.dir') + File.separator + System.getProperty('serverHome');
 
 println 'Configuring server on port 8080'
 port 8080
